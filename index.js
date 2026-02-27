c0e9767d5d6c1235ca3d029cb45f2999a2623f99
ashutosh26l <aksmba2611@gmail.com>
2026-02-28 00:32:42 +0530

    checking after restarting the server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080; // set the port
 
 // define a route for the root URL
 app.get('/', (req, res) => {
-  return res.json({msg: 'Hello from the server! final attempt'}); // send a JSON response
+  return res.json({msg: 'Hello from the server! checking after restarting the server'}); // send a JSON response
 });
 
 app.listen(PORT, () => {
